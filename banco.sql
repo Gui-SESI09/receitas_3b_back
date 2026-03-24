27697a51adefc68161a22613e20dd7606c587ddc
GUILHERME DALSASSO MACHADO <guilherme_machado18@estudante.sesisenai.org.br>
2026-03-24 11:51:09 -0300

    24/03 11:50

--- banco.sql
+++ banco.sql
@@ -1,17 +1,21 @@
-CREATE TABLE USUARIO (
+CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
-    senha VARCHAR(255),
-    email VARCHAR(255),
-    ativo BOOLEAN DEFAULT TRUE,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE RECEITA (
+CREATE TABLE chamados (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
-    ingredientes TEXT NOT NULL,
-    instrucoes TEXT NOT NULL,
-    tempo_preparo_minutos INTEGER NOT NULL,
-    usuario_id INTEGER NOT NULL REFERENCES USUARIO(id) ON DELETE CASCADE
+    titulo VARCHAR(150) NOT NULL,
+    descricao TEXT,
+    status VARCHAR(20) DEFAULT 'Aberto',
+    usuario_id INTEGER NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    
+    CONSTRAINT fk_usuario 
+        FOREIGN KEY(usuario_id) 
+        REFERENCES usuarios(id) 
+        ON DELETE CASCADE
 );
